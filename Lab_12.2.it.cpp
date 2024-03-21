547818b1d2918be05b09a4be61536b76b8290b19
Mstyslav Kruhlitskyi <wotmstislav@gmail.com>
2024-03-21 19:35:16 +0200

    stable

---
+++ Lab_12.2.it.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+using namespace std;
+
+typedef int Info;
+struct Elem
+{
+	Elem *link;
+	Info info;
+};
+
+void enqueue(Elem*& first, Elem*& last, Info value)
+{
+	Elem* tmp = new Elem;
+	tmp->info = value;
+	tmp->link = NULL;
+	if (last != NULL)
+		last->link = tmp;
+	last = tmp;
+	if (first == NULL)
+		first = tmp;
+}
+Info dequeue(Elem*& first, Elem*& last)
+{
+	Elem* tmp = first->link;
+	Info value = first->info;
+	delete first;
+	first = tmp;
+	if (first == NULL)
+		last = NULL;
+	return value;
+}
+bool check_pair_elements(Elem* L)
+{
+	while (L != NULL && L->link != NULL)
+	{
+		if (L->info == L->link->info)
+			return true;
+		L = L->link;
+	}
+	return false;
+}
+
+int main()
+{
+	Elem *first = NULL,
+		 *last = NULL;
+	
+	const int VALUE = 6;
+
+	cout << "Length list: " << VALUE << endl;
+	for (int i = 1; i <= VALUE; i++)
+	{
+		int n;
+		cout << "Enter values: "; cin >> n;
+		enqueue(first, last, n);
+	}
+	
+	cout << endl;
+	if (check_pair_elements(first))
+		cout << "Pair founded" << endl;
+	else
+		cout << "Pair not founded" << endl;
+
+
+	cout << endl;
+	while (first != NULL)
+		cout << dequeue(first, last) << " ";
+	cout << endl;
+
+	return 0;
+}
